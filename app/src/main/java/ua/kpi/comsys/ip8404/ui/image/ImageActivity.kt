fd7414ea8ada650310b282f6d47114d19e4f1b15
vano7577 <vano7577@gmail.com>
2021-05-26 16:28:58 +0300

    la6_update

--- app/src/main/java/ua/kpi/comsys/ip8404/ui/image/ImageActivity.kt
+++ app/src/main/java/ua/kpi/comsys/ip8404/ui/image/ImageActivity.kt
@@ -41,7 +41,7 @@ class ImageActivity : Fragment() {
 
         spannedGridLayoutManager.spanSizeLookup =
             SpannedGridLayoutManager.SpanSizeLookup { position ->
-                when (position % 9) {
+                when (position % 10) {
                     1 -> SpanSize(2, 2)
                     5 -> SpanSize(2, 2)
                     else ->
